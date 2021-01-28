2239fb761ebae05d48fda9c1642114315e8aacb6
marian muzyka <mrjnmzk@gmail.com>
2021-01-28 19:01:18 +0200

    add hw03

---
+++ homework03/01.js
@@ -0,0 +1,17 @@
+let a = [2, 3, 4, 5];
+let n = 1;
+
+for (i = 0; i < a.length; n++) { 
+    n *= a[i];
+}
+
+console.log(n);
+
+
+
+
+while (i < a.lenght) { 
+    n *= array[i]
+}
+
+console.log(n);
