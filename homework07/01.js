65b0bb84ae4383e0092c9f4a9b9135957dd85b5b
marian muzyka <mrjnmzk@gmail.com>
2021-02-18 14:01:11 +0200

    task 3

--- homework07/01.js
+++ homework07/01.js
@@ -32,3 +32,28 @@ myButton.addEventListener("click", changeCSS);
 
 // task 3
 
+function colorMaker(event) {
+    let color = document.body.style.backgroundColor
+
+    if (event.type == "click" && event.target.id == "button01") {
+        color = "blue";
+    } else if (event.type == "dblclick" && event.target.id == "button02") {
+        color = "pink";
+    } else if (event.type == "mousedown" && event.target.id == "button03") {
+        color = "brown";
+    } else if (event.type == "mouseup" && event.target.id == "button03") {
+        color = "white";
+    } else if (event.type == "mouseover" && event.target.id == "link") {
+        color = "yellow";
+    } else if (event.type == "mouseout" && event.target.id == "link") {
+        color = "white";
+    }
+    document.body.style.backgroundColor = color;
+}
+button01.addEventListener("click", colorMaker);
+button02.addEventListener("dblclick", colorMaker);
+button03.addEventListener("mousedown", colorMaker);
+button03.addEventListener("mouseup", colorMaker);
+
+link.addEventListener("mouseover", colorMaker);
+link.addEventListener("mouseout", colorMaker);
